df8d66b7e393b2efb58c3644360946610030631e
Chevon Christie <chevonc@live.com>
2019-07-21 18:08:28 -0700

    fix cli exit

--- src/cliEntry.js
+++ src/cliEntry.js
@@ -4,42 +4,42 @@
  *
  * @flow
  */
-import type { Command } from './types';
+import type { Command } from "./types";
 
-const minimist = require('minimist');
-const camelcaseKeys = require('camelcase-keys');
-const clear = require('clear');
-const inquirer = require('inquirer');
-const path = require('path');
-const chalk = require('chalk');
+const minimist = require("minimist");
+const camelcaseKeys = require("camelcase-keys");
+const clear = require("clear");
+const inquirer = require("inquirer");
+const path = require("path");
+const chalk = require("chalk");
 
-const pjson = require('../package.json');
-const logger = require('./logger');
-const messages = require('./messages');
-const { MessageError } = require('./errors');
+const pjson = require("../package.json");
+const logger = require("./logger");
+const messages = require("./messages");
+const { MessageError } = require("./errors");
 
-const DEFAULT_COMMAND = require('./commands/start');
+const DEFAULT_COMMAND = require("./commands/start");
 
 const COMMANDS: Array<Command> = [
-  require('./commands/init'),
-  require('./commands/start'),
-  require('./commands/bundle'),
-  require('./commands/reload'),
+  require("./commands/init"),
+  require("./commands/start"),
+  require("./commands/bundle"),
+  require("./commands/reload")
 ];
 
 const NOT_SUPPORTED_COMMANDS = [
-  'run-ios',
-  'run-android',
-  'library',
-  'unbundle',
-  'link',
-  'unlink',
-  'install',
-  'uninstall',
-  'upgrade',
-  'log-android',
-  'log-ios',
-  'dependencies',
+  "run-ios",
+  "run-android",
+  "library",
+  "unbundle",
+  "link",
+  "unlink",
+  "install",
+  "uninstall",
+  "upgrade",
+  "log-android",
+  "log-ios",
+  "dependencies"
 ];
 
 const getDisplayName = (command: string, opts: { [key: string]: mixed }) => {
@@ -48,25 +48,25 @@ const getDisplayName = (command: string, opts: { [key: string]: mixed }) => {
   const {
     npm_execpath: execPath,
     npm_lifecycle_event: scriptName,
-    npm_config_argv: npmArgv,
+    npm_config_argv: npmArgv
   } = process.env;
 
   // Haul has been called directly
   if (!execPath || !scriptName || !npmArgv) {
-    return `haul ${command} ${list.join(' ')}`;
+    return `haul ${command} ${list.join(" ")}`;
   }
 
-  const client = path.basename(execPath) === 'yarn.js' ? 'yarn' : 'npm';
+  const client = path.basename(execPath) === "yarn.js" ? "yarn" : "npm";
 
-  if (client === 'npm') {
+  if (client === "npm") {
     const argv = JSON.parse(npmArgv).original;
 
     return [
-      'npm',
-      ...(argv.includes('--') ? argv.slice(0, argv.indexOf('--')) : argv),
-      '--',
-      ...list,
-    ].join(' ');
+      "npm",
+      ...(argv.includes("--") ? argv.slice(0, argv.indexOf("--")) : argv),
+      "--",
+      ...list
+    ].join(" ");
   }
 
   // Yarn doesn't have `npmArgv` support
@@ -79,7 +79,7 @@ const getDisplayName = (command: string, opts: { [key: string]: mixed }) => {
       ? `yarn run ${scriptName}`
       : `yarn run ${scriptName} ${command}`;
 
-  return `${exec} -- ${list.join(' ')}`;
+  return `${exec} -- ${list.join(" ")}`;
 };
 
 async function validateOptions(options, flags) {
@@ -88,36 +88,36 @@ async function validateOptions(options, flags) {
 
   for (const option of options) {
     const defaultValue =
-      typeof option.default === 'function'
+      typeof option.default === "function"
         ? option.default(acc)
         : option.default;
 
     const parse =
-      typeof option.parse === 'function' ? option.parse : val => val;
+      typeof option.parse === "function" ? option.parse : val => val;
 
     let value = flags[option.name] ? parse(flags[option.name]) : defaultValue;
 
-    const missingValue = option.required && typeof value === 'undefined';
+    const missingValue = option.required && typeof value === "undefined";
     const invalidOption =
       option.choices &&
-      typeof value !== 'undefined' &&
-      typeof option.choices.find(c => c.value === value) === 'undefined';
+      typeof value !== "undefined" &&
+      typeof option.choices.find(c => c.value === value) === "undefined";
 
     if (missingValue || invalidOption) {
-      const message = option.choices ? 'Select' : 'Enter';
+      const message = option.choices ? "Select" : "Enter";
 
       // eslint-disable-next-line no-await-in-loop
       const question = await inquirer.prompt([
         {
-          type: option.choices ? 'list' : 'input',
-          name: 'answer',
+          type: option.choices ? "list" : "input",
+          name: "answer",
           message: `${message} ${option.description.toLowerCase()}`,
           choices: (option.choices || []).map(choice => ({
             name: `${String(choice.value)} - ${choice.description}`,
             value: choice.value,
-            short: choice.value,
-          })),
-        },
+            short: choice.value
+          }))
+        }
       ]);
 
       value = option.choices ? question.answer : parse(question.answer);
@@ -133,15 +133,15 @@ async function validateOptions(options, flags) {
 
 async function run(args: Array<string>) {
   if (
-    args[0] === 'version' ||
-    args.includes('-v') ||
-    args.includes('--version')
+    args[0] === "version" ||
+    args.includes("-v") ||
+    args.includes("--version")
   ) {
     console.log(`v${pjson.version}`);
     return;
   }
 
-  if (['--help', '-h', 'help'].includes(args[0])) {
+  if (["--help", "-h", "help"].includes(args[0])) {
     console.log(messages.haulHelp(COMMANDS));
     return;
   }
@@ -153,7 +153,7 @@ async function run(args: Array<string>) {
 
   const command = COMMANDS.find(cmd => cmd.name === args[0]) || DEFAULT_COMMAND;
 
-  if (args.includes('--help') || args.includes('-h')) {
+  if (args.includes("--help") || args.includes("-h")) {
     console.log(messages.haulCommandHelp(command));
     return;
   }
@@ -162,7 +162,7 @@ async function run(args: Array<string>) {
 
   const { _, ...flags } = camelcaseKeys(
     minimist(args, {
-      string: opts.map(opt => opt.name),
+      string: opts.map(opt => opt.name)
     })
   );
 
@@ -178,6 +178,9 @@ async function run(args: Array<string>) {
 
   try {
     await command.action(options);
+    if (!options.dev) {
+      process.exit(0);
+    }
   } catch (error) {
     clear();
     if (error instanceof MessageError) {
@@ -187,7 +190,7 @@ async function run(args: Array<string>) {
         messages.commandFailed({
           command: displayName,
           error,
-          stack: error && error.stack,
+          stack: error && error.stack
         })
       );
     }
